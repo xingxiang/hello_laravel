56970155a1d6fabed596c0ac2a3f8c9d15278376
xingxiang <xingxiangmin@yeah.net>
2017-03-22 07:51:35 +0000

    finish layout and routes

--- Laravel/app/Http/routes.php
+++ Laravel/app/Http/routes.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+get('/', 'StaticPagesController@home')->name('home');
+get('/help', 'StaticPagesController@help')->name('help');
+get('/about', 'StaticPagesController@about')->name('about');
 
-//Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+get('signup', 'UsersController@create')->name('signup');
---
+++ Laravel/resources/views/layouts/_footer.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-12">
+    <footer class="footer">
+        <small class="slogon">
+                创造不息，交付不止
+        </small>
+        <nav>
+            <ul>
+                <li><a href="{{ route('about') }}">关于</a></li>
+            </ul>
+        </nav>
+    </footer>
+</div>
